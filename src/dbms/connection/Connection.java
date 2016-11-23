09b30b57155c9e215dd54832ecf6d91ce7c063e2
Anas Harby <anas.harby@gmail.com>
2016-11-23 12:51:47 +0200

    Configured Interface

--- src/dbms/connection/Connection.java
+++ src/dbms/connection/Connection.java
@@ -11,33 +11,34 @@ public interface Connection {
 
 	public void dropDatabase(String dbName);
 
-	public void createTable(String tableName);
+	public void createTable(String dbName, String tableName,
+			HashMap<String, Class> columns);
 
 	public void dropTable(String tableName);
 
-	public void insertIntoTable(String tableName,
-			HashMap<Object, Object> entryMap);
+	public void insertIntoTable(String dbName, String tableName,
+			HashMap<String, Object> entryMap);
 
-	public Result selectAll(String tableName);
+	public Result selectAll(String dbName, String tableName);
 
-	public Result selectAll(String tableName,
+	public Result selectAll(String dbName, String tableName,
 			Condition condition);
 
-	public Result select(String tableName,
+	public Result select(String dbName, String tableName,
 			Collection<String> columns);
 
-	public Result select(String tableName,
+	public Result select(String dbName, String tableName,
 			Collection<String> columns, Condition condition);
 
-	public void delete(String tableName,
+	public void delete(String dbName, String tableName,
 			Collection<String> columns);
 
-	public void delete(String tableName,
+	public void delete(String dbName, String tableName,
 			Collection<String> columns, Condition condition);
 
-	public void update(String tableName,
+	public void update(String dbName, String tableName,
 			Collection<String> columns);
 
-	public void update(String tableName,
+	public void update(String dbName, String tableName,
 			Collection<String> columnns, Condition condition);
 }
