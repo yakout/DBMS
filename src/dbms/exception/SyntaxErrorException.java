2628b7c5eb927a78543238783a93a3519b203126
Ahmed Yakout <iyakout@hotmail.com>
2016-11-24 02:17:06 +0200

    finished all regex for sql syntax

---
+++ src/dbms/exception/SyntaxErrorException.java
@@ -0,0 +1,7 @@
+package dbms.exception;
+
+/**
+ * Created by khlailmohammedyakout on 11/23/16.
+ */
+public class SyntaxErrorException extends RuntimeException {
+}
---
+++ src/dbms/sqlparser/sqlInterpreter/rules/UseDatabase.java
@@ -0,0 +1,7 @@
+package dbms.sqlparser.sqlInterpreter.rules;
+
+/**
+ * Created by khlailmohammedyakout on 11/24/16.
+ */
+public class UseDatabase {
+}
