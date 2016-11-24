6aa8c4ca0858e0b41a77b80e092dd0564089672e
Anas Harby <anas.harby@gmail.com>
2016-11-24 18:33:12 +0200

    Merge with xml branch

--- src/dbms/test/Test.java
+++ src/dbms/test/Test.java
@@ -1,33 +1,42 @@
 package dbms.test;
 
-import dbms.util.ResultSet;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.transform.TransformerException;
+
+import dbms.exception.DatabaseAlreadyCreatedException;
+import dbms.exception.DatabaseNotFoundException;
+import dbms.exception.SyntaxErrorException;
+import dbms.exception.TableAlreadyCreatedException;
+import dbms.exception.TableNotFoundException;
+import dbms.xml.XMLParser;
 
 public class Test {
 	public static void main(String[] args) {
-//		try {
-//			XMLParser.getInstance().createDatabase("testDB");
-//		} catch (DatabaseAlreadyCreatedException e) {
-//			e.printStackTrace();
-//		}
-//		Map<String, Class> columns = new HashMap<>();
-//		columns.put("ID", Integer.class);
-//		columns.put("Name", String.class);
-//		columns.put("Organization", String.class);
-//		try {
-//			XMLParser.getInstance().createTable("testDB", "table1", columns);
-//		} catch (DatabaseNotFoundException | TableAlreadyCreatedException | TransformerException | SyntaxErrorException e) {
-//			e.printStackTrace();
-//		}
-//		Map<String, Object> rows = new HashMap<>();
-//		rows.put("ID", 125);
-//		rows.put("Name", "hamada14");
-//		rows.put("Organization", "AlexU");
-//		try {
-//			XMLParser.getInstance().insertIntoTable("testDB", "table1", rows);
-//		} catch (DatabaseNotFoundException | TableNotFoundException | SyntaxErrorException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		ResultSet set = new ResultSet();
+		try {
+			XMLParser.getInstance().createDatabase("testDB");
+		} catch (DatabaseAlreadyCreatedException e) {
+			e.printStackTrace();
+		}
+		Map<String, Class> columns = new HashMap<>();
+		columns.put("ID", Integer.class);
+		columns.put("Name", String.class);
+		columns.put("Organization", String.class);
+		try {
+			XMLParser.getInstance().createTable("testDB", "table1", columns);
+		} catch (DatabaseNotFoundException | TableAlreadyCreatedException | TransformerException | SyntaxErrorException e) {
+			e.printStackTrace();
+		}
+		Map<String, Object> rows = new HashMap<>();
+		rows.put("ID", 125);
+		rows.put("Name", "hamada14");
+		rows.put("Organization", "AlexU");
+		try {
+			XMLParser.getInstance().insertIntoTable("testDB", "table1", rows);
+		} catch (DatabaseNotFoundException | TableNotFoundException | SyntaxErrorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
